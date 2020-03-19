07be86ab9cbbfd6b63639fb734dea1a2ec05e062
rickyperdec <rickyperdec@gmail.com>
2020-03-19 21:57:52 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,58 @@
+<?php get_header(); ?>
+<section class="container-fluid aboutbg text-center">
+        <div class="container">
+          
+                <h2> GAMES &amp; LEVEL DESIGNER </h2>
+                <p class="particle">...and particle wizard</p>
+                <p class="about-text"> Hey my name is Josh Whitkin. Placeholder text placeholder text placeholder text placeholder text placeholder text. Placeholder text placeholder text placeholder text placeholder text placeholder text. </p>
+          
+            <div class="blocker"></div>
+            
+          </div> 
+          
+          
+    </section>
+      
+      <section class="container-fluid articlebg">
+        <div class="container">
+            <div class="row">
+                    <div>
+                        <h3 class="text-center article-title"> LATEST ARTICLES </h3>       
+                    </div>
+            
+            <div class="col-md-4 article-cent">
+                <img src="images/portrait.jpg" class="center" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ portrait.jpg">
+                <h4 class="port"> A PORTRAIT </h4>
+                <p class="date"> 1 March 2020 </p>  
+                    <p>
+                     Hey my name is Josh. Placeholder text placeholder text placeholder text placeholder text placeholder text. Placeholder text placeholder text placeholder text placeholder text placeholder text.   
+                    </p>
+                <p class="readmore"> CONTINUE READING </p>
+            </div>
+                
+            <div class="col-md-4 article-cent">
+                <img src="images/portrait.jpg" class="center" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/model.jpg">
+                <h4 class="port"> A MODEL </h4>
+                <p class="date"> 1 March 2020 </p>  
+                    <p>
+                     Hey my name is Josh. Placeholder text placeholder text placeholder text placeholder text placeholder text. Placeholder text placeholder text placeholder text placeholder text placeholder text.   
+                    </p>
+                <p class="readmore"> CONTINUE READING </p>
+            </div>
+                
+            <div class="col-md-4 article-cent">
+                <img src="images/portrait.jpg" class="center" src="http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/ game.jpg">
+                <h4 class="port"> GAME DESIGN </h4>
+                <p class="date"> 1 March 2020 </p>  
+                    <p>
+                     Hey my name is Josh. Placeholder text placeholder text placeholder text placeholder text placeholder text. Placeholder text placeholder text placeholder text placeholder text placeholder text.   
+                    </p>
+                <p class="readmore"> CONTINUE READING </p>
+            </div>
+                
+                
+            
+            </div>
+        </div>
+      </section>
+<?php get_footer(); /* Tells WordPress to include footer.php */ ?>
