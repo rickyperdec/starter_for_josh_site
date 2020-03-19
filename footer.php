477e2b8b6fd52d34da507123afe2ec05ef296908
rickyperdec <rickyperdec@gmail.com>
2020-03-19 21:57:40 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,20 @@
+<footer class="container-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 text-center footer-content">
+<p> <?php bloginfo('name'); ?></p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/tumblr.jpg" alt=“tumblr” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/rss.jpg" alt=“rss” >
+            </div>
+            <div class="col-md-6 text-center footer-content">
+                <p>Terms and Conditions</p>
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished//images/linkedin.jpg" alt=“linkedin” >
+                <img src=" http://206.189.45.97/~mesh1/wp-content/themes/josh_theme_wp_fiinished/images/fb.jpg" alt=“facebook” >
+            </div>
+
+        </div>
+    </div>
+</footer>              
+</body>
+</html>
+<?php wp_footer(); ?>
